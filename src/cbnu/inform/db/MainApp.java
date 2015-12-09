9ee56b0f6b3f68f044d5a93ee1c82434c4612ec4
Son <jujakhana@gmail.com>
2015-12-09 17:25:45 +0900

    하위 AnchorPane에 fxml 변경 코드 삽입

--- src/cbnu/inform/db/MainApp.java
+++ src/cbnu/inform/db/MainApp.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -50,9 +53,28 @@ public class MainApp extends Application {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource("view/MainOverview.fxml"));
-			AnchorPane mainOverview;
+			AnchorPane mainOverview;		
 			mainOverview = (AnchorPane) loader.load();
+			
+			FXMLLoader cloader = new FXMLLoader();
+			cloader.setLocation(MainApp.class.getResource("view/RegisterMainLayout.fxml"));
+			AnchorPane cpane = (AnchorPane) cloader.load();
+			
+			TabPane tpan = (TabPane)mainOverview.getChildren().get(0);
+			Tab tab = tpan.getTabs().get(0);
+			SplitPane spane = (SplitPane)tab.getContent();
+			AnchorPane ppane = (AnchorPane)spane.getItems().get(1);
+			
+			//spane.getItems().set(1, cpane);
+			//(AnchorPane)spane.getItems().get(1) = cpane;
+		
+			spane.getItems().set(1, cpane);
+			ppane.resize(primaryStage.getWidth(), primaryStage.getHeight());
 			rootLayout.setCenter(mainOverview);
+			
+			System.out.println(ppane.getId());
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
